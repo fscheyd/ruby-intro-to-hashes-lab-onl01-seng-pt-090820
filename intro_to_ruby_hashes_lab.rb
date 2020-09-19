b14945a4f5ed34447ab2128704487df60eaf7afa
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-09-19 17:25:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,27 +1,72 @@
 def new_hash
-
+  hashing_through_the_snow = { }
 end
 
 def actor
-
+  the_rock = {
+    name: "Dwayne The Rock Johnson"
+  }
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {
+	  railroads: {}
+	}
 
 end
 
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+	monopoly = {
+	  railroads: { 
+	    pieces: 4,
+	    names: {},
+	    rent_in_dollars: {}
+	   
+	  }
+	}
+	
+	
 end
 
 def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+	monopoly = {
+	  railroads: { 
+	    pieces: 4,
+	    names: {},
+	    rent_in_dollars: {}
+	   
+	  }
+	}
+	
+	monopoly[:railroads][:rent_in_dollars][:one_piece_owened] = 25
+	monopoly[:railroads][:rent_in_dollars][:two_pieces_owened] = 50
+	monopoly[:railroads][:rent_in_dollars][:three_pieces_owened] = 100
+	monopoly[:railroads][:rent_in_dollars][:four_pieces_owened] = 200
+	monopoly[:railroads][:names][:reading_railroad] = {}
+	monopoly[:railroads][:names][:pennsylvania_railroad] = {}
+	monopoly[:railroads][:names][:b_and_o_railroad] = {}
+  
+  monopoly
 end
 
 def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+	monopoly = {
+	  railroads: { 
+	    pieces: 4,
+	    names: {},
+	    rent_in_dollars: {}
+	   
+	  }
+	}
+	
+	monopoly[:railroads][:rent_in_dollars][:one_piece_owened] = 25
+	monopoly[:railroads][:rent_in_dollars][:two_pieces_owened] = 50
+	monopoly[:railroads][:rent_in_dollars][:three_pieces_owened] = 100
+	monopoly[:railroads][:rent_in_dollars][:four_pieces_owened] = 200
+	monopoly[:railroads][:names][:reading_railroad] = {}
+	monopoly[:railroads][:names][:pennsylvania_railroad] = {}
+	monopoly[:railroads][:names][:b_and_o_railroad] = {}
+  
+  monopoly
 
 end
