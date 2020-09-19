4a7b6fed362d541767b8c495f6fb9c6e4793495c
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-09-19 18:20:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,12 +38,12 @@ def monopoly_with_third_tier
 	  }
 	}
 	
-	:rent_in_dollars=>
+	{:rent_in_dollars =
 		{
-			:one_piece_owned=>25,
-			:two_pieces_owned=>50,
-			:three_pieces_owned=>100,
-			:four_pieces_owned=>200
+			:one_piece_owned = 25,
+			:two_pieces_owned = 50,
+			:three_pieces_owned = 100,
+			:four_pieces_owned = 200
 		}
 	monopoly[:railroads][:names][:reading_railroad] = {}
 	monopoly[:railroads][:names][:pennsylvania_railroad] = {}
